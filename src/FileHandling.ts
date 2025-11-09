18604e0b65248c1e1159e1c9d546331dfe48b299
wizcoderr <ssayam200@gmail.com>
2025-11-09 19:14:52 +0530

    fixex

--- src/FileHandling.ts
+++ src/FileHandling.ts
@@ -1,16 +1,23 @@
 import { promises } from 'fs'
 import { ProcessStack } from './DataType'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const tempDir = path.join(__dirname, 'temp');
+const filePath = path.join(tempDir, 'stack.json');
+const reportPath = path.join(tempDir, 'report.txt');
 
 
 export async function loadFile() {
-    const filePath = './src/temp/stack.json'
     try {
         let data = await promises.readFile(filePath, 'utf8')
         return JSON.parse(data) as ProcessStack
     } catch (error: any) {
         if (error.code === 'ENOENT') {
             try {
-                await promises.mkdir('./src/temp', { recursive: true });
+                await promises.mkdir(tempDir, { recursive: true });
             } catch (dirError) {
                 console.error("Error creating directory:", dirError);
                 throw dirError;
@@ -32,7 +39,7 @@ export async function loadFile() {
 
 export async function saveFile(data: ProcessStack) {
     const json = JSON.stringify(data, null, 4)
-    await promises.writeFile('./src/temp/stack.json', json)
+    await promises.writeFile(filePath, json)
 }
 
 
@@ -40,5 +47,5 @@ export async function submitReport(data: string) {
     const options = { timeZone: 'Asia/Kolkata' };
     const date = new Date()
     const text = `${data} \n ----------------- ${date.toLocaleDateString('en-IN', options)} | ${date.toLocaleTimeString('en-IN', options)}----------------- \n\n\n\n`
-    await promises.appendFile('./src/temp/report.txt', text)
+    await promises.appendFile(reportPath, text)
 }
